8e2b0ede1f6d79d8cc3fbf73ef7847ae18eda41f
ph34ktr4 <chhaysopheaktra41@gmail.com>
2025-08-05 11:57:15 +0700

    Delete src directory

--- src/app/convert-to-khr-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ConvertToKhrPipe } from './convert-to-khr-pipe';
-
-describe('ConvertToKhrPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ConvertToKhrPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
